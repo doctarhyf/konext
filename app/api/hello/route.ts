94cb45cc0e78c576e5507d9e91818708268715f2
Docta Rhyf <drrhyf@gmail.com>
2024-03-15 21:46:57 +0200

    init

---
+++ app/api/hello/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const name = url.searchParams.get("name");
+
+  return NextResponse.json({ text: "Hello" });
+}
