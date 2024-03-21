8dc4a3bde6ddc99ba856f37fc2a1c5d6e16bb352
Docta Rhyf <drrhyf@gmail.com>
2024-03-21 21:33:57 +0200

    api upd

--- app/api/auth/login/route.tsx
+++ app/api/auth/login/route.tsx
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as SB from "@/db/db";
 import { TABLE_NAMES } from "@/db/supabase";
+import { User } from "@/db/types";
 
 export const dynamic = "force-dynamic";
 
@@ -11,7 +12,20 @@ export async function POST(req: NextRequest) {
   if (creds) {
     const userData = await SB.loadSingleItemByPhoneAndPin(phone, pin);
 
-    console.log("userData => ", userData);
+    if (userData === null) {
+      const res = (await (SB.loadAllItemsWithCondition(
+        TABLE_NAMES.KOOP_USERS,
+        "phone",
+        phone
+      ) as unknown)) as User;
+      if (res && res.id) {
+        return NextResponse.json(
+          { error: true, message: "PIN INCORRECT" },
+          { status: 500 }
+        );
+      }
+    }
+
     return NextResponse.json(userData, { status: 200 });
   }
 
