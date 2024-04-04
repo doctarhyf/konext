961019372dbb556879d144a854d80fbbf37717e4
Docta Rhyf <drrhyf@gmail.com>
2024-04-04 10:14:53 +0200

    fixing sreq route

--- app/api/sreq/route.tsx
+++ app/api/sreq/route.tsx
@@ -8,59 +8,57 @@ export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
   const params = new URL(req.url).searchParams;
   const user_id = params.get("user_id");
-  let serviceRequestArray: ServiceRequest[] = [];
+  const finalSReqsArray: ServiceRequest[] = [];
 
   //console.error("user id => ", user_id);
 
   try {
-    let promise = SB.loadAllItems(TABLE_NAMES.KOOP_SERVICE_REQUEST);
-
-    if (user_id) {
-      promise = SB.loadAllItemsWithCondition(
-        TABLE_NAMES.KOOP_SERVICE_REQUEST,
-        "user_id",
-        user_id
-      );
-    }
-
-    const sreqs = (await promise) as ServiceRequest[];
-
     const users_ids: number[] = [];
-
-    const readingAll = user_id === null;
     const users_by_id: Record<number, User> = {};
-    const final_items: ServiceRequest[] = [];
-    if (readingAll) {
-      sreqs.forEach((sr, i) => {
-        const { user_id } = sr;
-        if (!users_ids.includes(user_id)) users_ids.push(user_id);
-      });
+    const readingAll = user_id === null;
+    const usersPromises: any = [];
 
-      const usersPromises: any = [];
-      if (users_ids.length > 0) {
-        users_ids.forEach((uid, i) => {
-          usersPromises.push(SB.loadItem(TABLE_NAMES.KOOP_USERS, "id", uid));
-        });
-      }
+    let promise = readingAll
+      ? SB.loadAllItems(TABLE_NAMES.KOOP_SERVICE_REQUEST)
+      : SB.loadAllItemsWithCondition(
+          TABLE_NAMES.KOOP_SERVICE_REQUEST,
+          "user_id",
+          user_id
+        );
 
-      const users = await Promise.all(usersPromises);
+    const rawServiceRequests = (await promise) as ServiceRequest[];
 
-      users.forEach((user, i) => {
-        users_by_id[user.id] = user;
-      });
+    //get users ids
+    rawServiceRequests.forEach((sreq, i) => {
+      const { user_id } = sreq;
+      if (!users_ids.includes(user_id)) users_ids.push(user_id);
+    });
 
-      //only save sreqs that have a valid user
-      //sreqs.map((sr, i) => (sr.user_data = users_by_id[sr.user_id]));
-      sreqs.forEach((sr, i) => {
-        const user_data = users_by_id[sr.user_id];
-
-        if (user_data) {
-          serviceRequestArray.push({ ...sr, user_data: user_data });
-        }
+    // setup users load promises
+    if (users_ids.length > 0) {
+      users_ids.forEach((uid, i) => {
+        usersPromises.push(SB.loadItem(TABLE_NAMES.KOOP_USERS, "id", uid));
       });
     }
 
-    return NextResponse.json(serviceRequestArray, { status: 200 });
+    const users = await Promise.all(usersPromises);
+
+    //sort users by user_id
+    users.forEach((user, i) => {
+      users_by_id[user.id] = user;
+    });
+
+    //only save sreqs that have a valid user
+    //sreqs.map((sr, i) => (sr.user_data = users_by_id[sr.user_id]));
+    rawServiceRequests.forEach((sr, i) => {
+      const user_data = users_by_id[sr.user_id];
+
+      if (user_data) {
+        finalSReqsArray.push({ ...sr, user_data: user_data });
+      }
+    });
+
+    return NextResponse.json(finalSReqsArray, { status: 200 });
   } catch (e) {
     NextResponse.json(
       { error: true, message: "Internal server error : " + JSON.stringify(e) },
