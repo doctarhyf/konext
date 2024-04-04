0dfc6f6e872b775246cbb8abd12a14e7f48df6ce
Docta Rhyf <drrhyf@gmail.com>
2024-04-04 07:02:35 +0200

    fixed sreq only return sreqs with valid user data

--- app/api/sreq/route.tsx
+++ app/api/sreq/route.tsx
@@ -49,47 +49,18 @@ export async function GET(req: NextRequest) {
         users_by_id[user.id] = user;
       });
 
-      sreqs.map((sr, i) => (sr.user_data = users_by_id[sr.user_id]));
+      //only save sreqs that have a valid user
+      //sreqs.map((sr, i) => (sr.user_data = users_by_id[sr.user_id]));
+      sreqs.forEach((sr, i) => {
+        const user_data = users_by_id[sr.user_id];
+
+        if (user_data) {
+          serviceRequestArray.push({ ...sr, user_data: user_data });
+        }
+      });
     }
 
-    /*
-    if ((user_id === null || user_id === undefined) && sreqs) {
-      const users_ids: number[] = [];
-
-      sreqs.forEach((it, i) => {
-        if (!users_ids.includes(it.user_id)) users_ids.push(it.user_id);
-      });
-
-      const promises: any = [];
-      if (users_ids.length > 0) {
-        users_ids.forEach((uid, i) => {
-          promises.push(SB.loadItem(TABLE_NAMES.KOOP_USERS, "id", uid));
-        });
-      }
-
-      const users = await Promise.all(promises);
-      console.error("loaded users => ", users);
-
-      const users_by_id: any = {};
-      users.forEach((user, i) => {
-        if (!users_by_id[user.id]) {
-          users_by_id[user.id] = [];
-        }
-        users_by_id[user.id] = user;
-      });
-
-      sreqs.forEach((sr, i) => {
-        const fsreq: ServiceRequest = {
-          ...sr,
-          user_data: users_by_id[sr.user_id],
-        };
-        serviceRequestArray.push(fsreq);
-      });
-    } */
-
-    // console.error(`ServReq => `, serviceRequestArray);
-
-    return NextResponse.json(sreqs, { status: 200 });
+    return NextResponse.json(serviceRequestArray, { status: 200 });
   } catch (e) {
     NextResponse.json(
       { error: true, message: "Internal server error : " + JSON.stringify(e) },
