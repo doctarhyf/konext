982ff5a2f8c5fb25f9071d817a5d6a49257a266b
Docta Rhyf <drrhyf@gmail.com>
2024-04-15 19:50:15 +0200

    dbg

--- app/api/sreq/remove/route.tsx
+++ app/api/sreq/remove/route.tsx
@@ -17,67 +17,10 @@ export async function GET(req: NextRequest) {
     );
   }
 
-  const r = await SB.removeServReq(item_id);
-  console.log(`Res remove item_id "${item_id}" => \n`, r);
-
-  return NextResponse.json(r, { status: 200 });
-
-  /* const finalSReqsArray: ServiceRequest[] = [];
-
-  //console.error("user id => ", user_id);
-
   try {
-    const users_ids: number[] = [];
-    const users_by_id: Record<number, User> = {};
-    const readingAll = user_id === null;
-    const usersPromises: any = [];
-
-    let promise = readingAll
-      ? SB.loadAllItems(TABLE_NAMES.KOOP_SERVICE_REQUEST)
-      : SB.loadAllItemsWithCondition(
-          TABLE_NAMES.KOOP_SERVICE_REQUEST,
-          "user_id",
-          user_id
-        );
-
-    const rawServiceRequests = (await promise) as ServiceRequest[];
-
-    //get users ids
-    rawServiceRequests.forEach((sreq, i) => {
-      const { user_id } = sreq;
-      if (!users_ids.includes(user_id)) users_ids.push(user_id);
-    });
-
-    // setup users load promises
-    if (users_ids.length > 0) {
-      users_ids.forEach((uid, i) => {
-        usersPromises.push(SB.loadItem(TABLE_NAMES.KOOP_USERS, "id", uid));
-      });
-    }
-
-    const users = await Promise.all(usersPromises);
-
-    //sort users by user_id
-    users.forEach((user, i) => {
-      users_by_id[user.id] = user;
-    });
-
-    //only save sreqs that have a valid user
-    //sreqs.map((sr, i) => (sr.user_data = users_by_id[sr.user_id]));
-    rawServiceRequests.forEach((sr, i) => {
-      const user_data = users_by_id[sr.user_id];
-
-      if (user_data) {
-        sr.timeAgo = timeAgo(new Date(sr.created_at));
-        finalSReqsArray.push({ ...sr, user_data: user_data });
-      }
-    });
-
-    return NextResponse.json(finalSReqsArray, { status: 200 });
+    const r = await SB.removeServReq(item_id);
+    return NextResponse.json(r, { status: 200 });
   } catch (e) {
-    NextResponse.json(
-      { error: true, message: "Internal server error : " + JSON.stringify(e) },
-      { status: 500 }
-    );
-  } */
+    return NextResponse.json(e, { status: 500 });
+  }
 }
