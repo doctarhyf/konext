af76a8ab4c3c993e4692ab1a6e6c529dc3b3b2b2
Docta Rhyf <drrhyf@gmail.com>
2025-05-22 22:37:57 +0200

    upd

--- app/bags/page.tsx
+++ app/bags/page.tsx
@@ -35,45 +35,107 @@ export default function Bags() {
     );
   }
 
+  const [errorStockOverflow, seterrorStockOverflow] = useState<boolean>(false);
   const [bags, setBags] = useState<number>(0);
-  const [bagType, setBagType] = useState<number>(300);
+  const [bagsInBao, setBagsInBao] = useState<number>(300);
+  const [bagType, setBagType] = useState<"s32" | "s42">("s32");
   const [total, setTotal] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const [team, setTeam] = useState<string | null>(null);
+  const [containerStock, setContainerStock] = useState<{
+    s32: number;
+    s42: number;
+  }>({
+    s32: 0,
+    s42: 0,
+  });
 
   useEffect(() => {
-    setTotal(bagType * bags);
-  }, [bags, bagType]);
+    fetchData();
+  }, []);
 
-  async function onSave(name: string, total: number) {
+  async function fetchData() {
+    setLoading(true);
+    const { data, error } = await supabase_shuini
+      .from("sacs_stock_cont")
+      .select("*")
+      .order("created_at", { ascending: false }) // or 'id'
+      .limit(1)
+      .single();
+
+    console.log("data cont", data);
+
+    if (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false);
+    } else {
+      setContainerStock({ s32: data.s32, s42: data.s42 });
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    let total = 0;
+
+    seterrorStockOverflow(false);
+
+    if (bagType === "s32") {
+      if (bags * bagsInBao > containerStock.s32) {
+        seterrorStockOverflow(true);
+      }
+    }
+
+    if (bagType === "s42") {
+      if (bags * bagsInBao > containerStock.s42) {
+        seterrorStockOverflow(true);
+      }
+    }
+
+    // if (!errorStockOverflow) {
+    total = bags * bagsInBao;
+    //  }
+    setTotal(total);
+  }, [bags, bagsInBao, bagType, containerStock, team]);
+
+  async function onSave(
+    fuzeren: string,
+    daizi: number,
+    team: string,
+    date_time: string
+  ) {
     setLoading(true);
 
     const data = {
-      fuzeren: name,
-      daizi: total,
-      team: "A",
-      date_time: "2025.05.22 20:30",
+      fuzeren: fuzeren,
+      daizi: daizi,
+      team: team,
+      date_time: date_time,
     };
     try {
-      const { data, error } = await supabase_shuini
-        .from("sacs_stock_cont")
-        .select("*")
-        .order("created_at", { ascending: false }) // or 'id'
-        .limit(1)
-        .single();
-
-      console.log("data", data);
-      alert("data :" + data);
-
       const res = await supabase_shuini
         .from("sacs_sortie_cont")
         .insert([data])
         .select();
 
-      alert(JSON.stringify(res));
+      const newStock32 =
+        bagType === "s32" ? containerStock.s32 - total : containerStock.s32;
+      const newStock42 =
+        bagType === "s42" ? containerStock.s42 - total : containerStock.s42;
+
+      const newStock = { s32: newStock32, s42: newStock42 };
+      setContainerStock(newStock);
+
+      const res2 = await supabase_shuini
+        .from("sacs_stock_cont")
+        .insert([newStock])
+        .select();
+
       // window.location.href = "https://gongren.vercel.app";
+      alert(JSON.stringify(res));
+      setLoading(false);
       return res.error ? { error: true, ...res.error } : res.data;
     } catch (e) {
+      setLoading(false);
       return e;
     }
 
@@ -82,6 +144,12 @@ export default function Bags() {
 
   return (
     <div className="main h-screen text-black">
+      <div>
+        <div>s32: {containerStock.s32}</div>
+        <div>s42: {containerStock.s42}</div>
+        {JSON.stringify(containerStock)}
+      </div>
+
       <div>{name}先生你好!</div>
       <div>您想令几包袋子</div>
       <div>包</div>
@@ -92,11 +160,28 @@ export default function Bags() {
           onChange={(e) => setBags(parseInt(e.target.value))}
         />
       </div>
-      <div>多少袋子的包</div>
+
+      <div>包 Type</div>
+
       <div>
         <select
           value={bagType}
-          onChange={(e) => setBagType(parseInt(e.target.value))}
+          onChange={(e) => setBagType(e.target.value as "s32" | "s42")}
+          title="bao"
+        >
+          {["s32", "s42"].map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div>多少袋子的包</div>
+      <div>
+        <select
+          value={bagsInBao}
+          onChange={(e) => setBagsInBao(parseInt(e.target.value))}
           title="bao"
         >
           {[300, 500].map((item) => (
@@ -123,7 +208,22 @@ export default function Bags() {
       <div>总和</div>
       <div className=" text-3xl ">{total} 袋</div>
 
-      <button onClick={(e) => onSave(name, total)}>CONFIRM</button>
+      {errorStockOverflow ? (
+        <div>
+          The number of bags to be delivered cant be superior to the available
+          stock
+        </div>
+      ) : (
+        !loading && (
+          <button
+            onClick={(e) => onSave(name, total, team ?? "", "2025.05.22")}
+          >
+            CONFIRM
+          </button>
+        )
+      )}
+
+      {loading && <div>Loading...</div>}
     </div>
   );
 }
